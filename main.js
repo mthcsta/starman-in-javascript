02f89e5d0b0cef86da3fbaa2a75e35a6a6f0a70d
mthcsta <matheuscostakrenn@gmail.com>
2019-12-20 19:52:29 -0300

    Add info to mobile

--- main.js
+++ main.js
@@ -414,21 +414,25 @@ function startMobile(){
     canvas.style.display = "block";
 }
 function modeMobile(){
+    div = document.createElement("div");
+    div.innerHTML = "Gire a tela na vertical e dê fullscreen para rodar o jogo";
+    div.style.color = "white";
+    div.style.padding = "10px";
     botao = document.createElement("button");
     botao.innerHTML = "Abrir FullScreen";
     botao.style.display="block";
     botao.style.margin = "auto";
     botao.addEventListener("click", function(){
-        botao.style.display="none";
+        div.style.display="none";
         fullscreen();
     });
-    document.body.appendChild(botao);
+    div.appendChild(botao);
+    document.body.appendChild(div);
 }
 
 
 document.onfullscreenchange = window.onresize = function(){ 
-    if(typeof document.onfullscreenchange == "undefined" || isFullscreen()){
-        console.log("a");
+    if(typeof document.onfullscreenchange == "undefined" || isFullscreen() && window.innerWidth > window.innerHeight){
         startMobile(); 
     }
     canvas.width = window.innerWidth;
